f4fd99499d07794570ec8e2ce12570f6c2706d00
ZalewskiPiotr <1piotrzalewski@gmail.com>
2024-08-04 12:58:34 +0200

    Zmiana organizacji metod
    W celu uporządkowania kodu wyciągnięto kod od kolizji do osobnej metody

--- scripts/necromancer.gd
+++ scripts/necromancer.gd
@@ -7,22 +7,11 @@ const SPEED = 300.0		# Szybkość gracza
 func _physics_process(delta: float):
 	set_player_speed()
 	move_and_slide()
-	var collision_count : int = get_slide_collision_count()
-	for i : int in collision_count:
-		var collision : KinematicCollision2D = get_slide_collision(i)
-		var collider : Object = collision.get_collider()
-		if collider.name == "BubbleScore":
-			pass
-			#collider.hit() # zawołamy metodę hit z BubbleScore
-			# z Necromancer wyemitujemy sygnał AddPoint. Ten sygnał złapie HUD i zaktualizuje punktację
-		elif collider.name == "BubbleKiller":
-			pass
-			# zawołamy metodę hit z BubbleKiller, która usunie bańkę
-			# wyemitujemy sygnał death
-			
+	detect_collision()
 #endregion
 
 
+#region Zarządzanie ruchem gracza - szybkośc, animacja, itd
 ## Funkcja ustawia szybkość gracza
 ##
 ## Szybkość ustawiana jest na podstawie kierunku poruszania się gracza. Kierunek ten jest 
@@ -32,3 +21,22 @@ func set_player_speed():
 			"player_move_up","player_move_down")
 	direction.normalized()
 	velocity = direction * SPEED
+#endregion
+
+
+#region Kolizje
+func detect_collision():
+	var collision_count : int = get_slide_collision_count()
+	for i : int in collision_count:
+		var collision : KinematicCollision2D = get_slide_collision(i)
+		var collider : Object = collision.get_collider()
+		if collider is BubbleScore:
+			print("Collider bubble score")
+			pass
+			#collider.hit() # zawołamy metodę hit z BubbleScore
+			# z Necromancer wyemitujemy sygnał AddPoint. Ten sygnał złapie HUD i zaktualizuje punktację
+		elif collider.name == "BubbleKiller":
+			pass
+			# zawołamy metodę hit z BubbleKiller, która usunie bańkę
+			# wyemitujemy sygnał death
+#endregion
